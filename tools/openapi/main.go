ba5ca3ddb4f964505dc2d00fb172a4a1acfc1594
Rémy LAVERGNE <lavergne.remy@gmail.com>
2021-07-01 14:40:46 +0200

    dead code

--- tools/openapi/main.go
+++ tools/openapi/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -26,7 +25,6 @@ func main() {
 		cmd := exec.Command("/bin/sh", "-c", "java -jar openapi-generator-cli.jar generate -g kotlin -i https://raw.githubusercontent.com/openapitools/openapi-generator/master/modules/openapi-generator/src/test/resources/3_0/petstore.yaml --library multiplatform --model-name-suffix Dto -o out/kotlin-multiplatform")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
-			fmt.Println("an error occurred.")
 			log.Fatal(err)
 		}
 
